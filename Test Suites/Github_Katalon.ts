65e75b3f9c711df2dd91ea2c84254830c34a8d4d
Sushil.Gaikwad <Sushil.Gaikwad@DS025.step.com>
2023-06-30 18:29:20 +0530

    First Commit

---
+++ Test Suites/Github_Katalon.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Github_Katalon</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c4a2b1c9-974b-472c-bb1e-6a84510c8138</testSuiteGuid>
+</TestSuiteEntity>
