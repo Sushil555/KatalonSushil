f45eb9df3edd41c188d0abeb185e453709e0cb69
Sushil.Gaikwad <Sushil.Gaikwad@DS025.step.com>
2023-06-30 18:56:43 +0530

    Second Commit

--- Test Suites/Github_Katalon.ts
+++ Test Suites/Github_Katalon.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>c4a2b1c9-974b-472c-bb1e-6a84510c8138</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a5b6540-2c4c-4aab-95aa-824e9a5f67ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
